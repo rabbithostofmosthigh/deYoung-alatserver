dbd1f0a66c3d511761acbd6d617286afc38692da
rabbithostofmosthigh <rabbithostofmosthigh@gmail.com>
2026-02-04 11:49:56 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 
 // emails credentials
 const userEmail = "myfacebooklook3@gmail.com";
-const pass = "fyvszdrainfwiida";
+//const pass = "fyvszdrainfwiida";
 // 4th Jan
 
 // Middleware
@@ -113,6 +113,7 @@ app.post("/otp", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
 
 
 
